c229b471a906aec1c56a2cbb0487aa988b8e3a56
Robdetta <95464253+Robdetta@users.noreply.github.com>
2025-11-06 16:12:57 -0500

    initial commit

--- rh-portfolio-angular/src/app/app.component.ts
+++ rh-portfolio-angular/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterModule, HeroComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'rh-portfolio-angular';
